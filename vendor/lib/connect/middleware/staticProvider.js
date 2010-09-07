70ffd783d6976f7028efa155202bc4d35f2959d4
Matthias Lübken <matthias.luebken@gmail.com>
2010-09-07 12:40:50 +0200

    Cache-Control

--- vendor/lib/connect/middleware/staticProvider.js
+++ vendor/lib/connect/middleware/staticProvider.js
@@ -108,6 +108,7 @@ module.exports = function staticProvider(root){
                     "Last-Modified": stat.mtime.toUTCString(),
                     // Cache in browser for 1 year
                     //"Cache-Control": "public max-age=" + 31536000
+                    "Cache-Control": "public max-age=0, must-revalidate"
                 });
                 res.end(data);
             }
