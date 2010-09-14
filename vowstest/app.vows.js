5c3b6f8d948c6cb812e06de04040490e13de57d6
Matthias Lübken <matthias.luebken@gmail.com>
2010-09-15 01:02:59 +0200

    test static routs with macros

--- vowstest/app.vows.js
+++ vowstest/app.vows.js
@@ -1,11 +1,31 @@
 require.paths.unshift('vendor/testlib');
 
 var vows = require('vows'),
-    assert = require('assert');    
+    assert = require('assert');   
 
 var http = require('http'),
-     app = require('../lib/app').app,
-     pageProvider = require('../lib/app').pageProvider;
+     app = require('../lib/app').app;
+
+//app.pageProvider = {};
+     
+app.listen(8000);
+var client = http.createClient(80, 'localhost');
+
+//helper
+function assertStatus(code) {
+    return function (res, y) {
+        assert.equal (res.statusCode, code);
+    };
+}
+var api = {
+    get: function (path) {
+        return function () {
+            var request = client.request('GET', path, {'host': 'localhost:8000'});
+            request.end();
+            request.on('response', this.callback);
+        };
+    }
+};
 
 // Create a Test Suite
 vows.describe('app').addBatch({
@@ -16,27 +36,21 @@ vows.describe('app').addBatch({
         }
     }
 }).addBatch({
-    'app serves static files': {
-        topic: function () {
-            app.listen(8000);
-            var client = http.createClient(80, 'localhost');
-            var request = client.request('GET', '/', {'host': 'localhost:8000'});
-            request.end();
-            request.on('response', this.callback);
-        },
-        'should respond with a 200 OK': function (res, y) {
-           assert.equal (res.statusCode, 200);
-        }
+    'app serves root file': {
+        topic: api.get('/'),
+        'should respond with a 200': assertStatus(200)
+    },
+    'app serves index file': {
+        topic: api.get('/index.html'),
+        'should respond with a 200': assertStatus(200)
     },
     'app doesnt serve unknown files': {
-        topic: function () {
-            var client = http.createClient(80, 'localhost');
-            var request = client.request('GET', '/sasd', {'host': 'localhost:8000'});
-            request.end();
-            request.on('response', this.callback);
-        },
-        'should respond with a 404': function (res, y) {
-           assert.equal (res.statusCode, 404);
-        }
+        topic: api.get('/asdasda'),
+        'should respond with a 404': assertStatus(404)
+    }
+}).addBatch({
+    'app doesnt serve unknown agenda ': {
+        topic: api.get('/agenda/unknownagenda'),
+        'should respond with a 404': assertStatus(404)
     }
 }).run(); // Run it
