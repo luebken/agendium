327f1a717c2f659ee7080d52db7bfb8af882a8be
Matthias Lübken <matthias.luebken@gmail.com>
2010-10-28 08:46:42 +0200

    no error if json parse fails

--- vowstest/app.vows.js
+++ vowstest/app.vows.js
@@ -4,7 +4,8 @@ var vows = require('vows'),
     assert = require('assert');   
 
 var http = require('http'),
-   app = require('../lib/app').app;
+   app = require('../lib/app').app,
+   pageprovider = require('../lib/app').pageProvider;
      
 var client;
 var app;
@@ -19,6 +20,15 @@ var api = {
     get: function (path) {
         return function () {
             var request = client.request('GET', path, {'host': 'localhost:8000'});
+            request.end();
+            request.on('response', this.callback);
+        };
+    },
+    post: function (path, body) {
+        return function () {
+            var request = client.request('POST', path, {'Content-Length': '27'});
+            if(body) request.write(body)
+            else request.write('{"bodyll":"s", "_id":"123"}');
             request.end();
             request.on('response', this.callback);
         };
@@ -62,10 +72,25 @@ vows.describe('app')
     }
 })
 
+.addBatch({
+    'app should accept a post to /agenda': {        
+        topic: api.post('/agenda'),
+        'should respond with a 200': assertStatus(200)
+    },
+    'app shouldnt accept a post to /bblala': {        
+        topic: api.post('/bblala'),
+        'should respond with a 404': assertStatus(404)
+    },
+    'app shouldnt accept a flawed json': {        
+        topic: api.post('/agenda', '{"bodyll":"s", "_id":"""123"}'),
+        'should respond with a 404': assertStatus(404)
+    }
+})
 
 .addBatch({
     'app is closed': {
         topic: function() {
+            pageprovider.close();
             app.close();
             app = null;
             return null;
